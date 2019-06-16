1276960089b09259c17a36ff702955921be55364
yuetongliu <liuyuetong1211@163.com>
2019-06-15 20:59:23 -0400

    Clean

--- Program/1.Clean_Stock_Data.R
+++ Program/1.Clean_Stock_Data.R
@@ -14,26 +14,21 @@ rm(list=ls())
 # source scripts: ------------
 # source(here("src", "ggtheme.R"))
 source("https://raw.githubusercontent.com/YuetongLiu/Prophet_Forecasting/master/Program/1.2weeknum_function.r")
-source("https://github.com/YuetongLiu/Prophet_Forecasting/blob/master/Program/1.1.generate-levels-for-month-year.r")
+source("https://raw.githubusercontent.com/YuetongLiu/Prophet_Forecasting/master/Program/1.1.generate-levels-for-month-year.r")
 
 
 # read ed data in: --------------------------------
 df1.stock <- read.csv("https://raw.githubusercontent.com/YuetongLiu/Prophet_Forecasting/master/Data/Stock.csv")
 
-# here and lubridate packages don't seem to play well, so we do this later
-
-df1.ed %<>% 
-  mutate(year = floor_date(StartDate,
-                           unit = "year") %>% 
+df1.stock %<>% 
+  mutate(year = floor_date(as.Date(StartDate), unit="year") %>% 
            as.character %>% 
            substr(1,4) %>% 
            as.factor, 
-         week = floor_date(StartDate, 
-                           unit = "week") %>% 
+         week = floor_date(as.Date(StartDate),unit="week")%>% 
            week.fn %>% 
            as.factor, 
-         month = floor_date(StartDate, 
-                            unit = "month") %>%
+         month = StartDate %>%
            as.character %>%
            substr(6,7) %>% 
            as.factor, 
@@ -42,28 +37,18 @@ df1.ed %<>%
                             sep="-") %>% 
            factor(levels = month.year.levels)) 
 
-# str(df1.ed)
-# summary(df1.ed); head(df1.ed)
-# as.data.frame(df1.ed)[1:100, ]
+# str(df1.stock)
+# summary(df1.stock); head(df1.stock)
+# as.data.frame(df1.stock)[1:100, ]
 
 
 
 # prepare data for prohet:------------
-df2.ed.prophet <- select(df1.ed, 
+df2.stock.prophet <- select(df1.stock, 
                          StartDate, 
-                         numvisits) %>% 
+                         DAX) %>% 
   rename(ds=StartDate, 
-         y=numvisits)
+         y=DAX)
 
-str(df2.ed.prophet)
+str(df2.stock.prophet)
 
-
-
-# write outputs: --------------
-unloadNamespace("lubridate")
-write_csv(df1.ed, 
-          paste0(here("output from src"), 
-                 "/df1-ed-reformatted.csv")) 
-
-# in xl, change month.year col to text then paste: 
-writeClipboard(df1.ed$month.year %>% as.character)
